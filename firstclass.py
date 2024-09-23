03d8e7fed94a78f462828b60bdece7e3c8f73605
kmandirsoftware <keith.gerhards@gmail.com>
2024-09-23 10:42:33 -0600

    added loop to handle all cards

--- firstclass.py
+++ firstclass.py
@@ -1,29 +1,23 @@
 import pygetwindow as gw
 from modules import hashhandler as hh 
 import suits
+import locations
 
-#Ignition_windows = gw.getWindowsWithTitle("No Limit Hold")
-Ignition_windows = gw.getWindowsWithTitle("Poker Lobby")
+Ignition_windows = gw.getWindowsWithTitle("No Limit Hold")
+#Ignition_windows = gw.getWindowsWithTitle("Poker Lobby")
 left, top, right, bottom = Ignition_windows[0].left, Ignition_windows[0].top, Ignition_windows[0].right, Ignition_windows[0].bottom
-#first suit
-l = left+344
-t = top +382
-r = left + 367
-b = top +402
 
-first_card_suit_position = (l,t,r,b)
-first_card_suit = hh.HashHandler(first_card_suit_position,suits.suit_hash)
-somehash1  = first_card_suit.take_screenshot()
-print(somehash1)
-first_card_suit.identify_hash(somehash1)
-#second suit
-l2 = left+382
-t2 = top +382
-r2 = left + 405
-b2 = top +402
-second_card_suit_position = (l2,t2,r2,b2)
-second_card_suit = hh.HashHandler(second_card_suit_position,suits.suit_hash)
-somehash  = second_card_suit.take_screenshot()
-print(somehash)
-second_card_suit.identify_hash(somehash)
+
+for card in locations.board:
+        icard = locations.board[card]
+        l = left + icard["left"]
+        t = top  + icard["top"]
+        r = left + icard["right"]
+        b = top  + icard["bottom"]
+        card_suit_poition = (l,t,r,b)
+        card_suit = hh.HashHandler(card_suit_poition,suits.suit_hash)
+        somehash = card_suit.take_screenshot()
+        print(card,somehash)
+        card_suit.identify_hash(somehash)
+
 
---
+++ locations.py
@@ -0,0 +1,44 @@
+board = {
+	'card1': {
+		'left': 344,
+		'top':382,
+		'right':367,
+		'bottom':402
+	},
+	'card2': {
+		'left':382,
+		'top':382,
+		'right':405,
+		'bottom':402
+	},
+	'flop1': {
+		'left':232,
+		'top':240,
+		'right':256,
+		'bottom':266	
+	},
+	'flop2': {
+		'left':292,
+		'top':240,
+		'right':310,
+	'bottom':265	
+	},
+	'flop3': {
+		'left':352,
+		'top':240,
+		'right':380,
+		'bottom':265	
+	},
+	'flop4': {
+		'left':410,
+		'top':240,
+		'right':428,
+		'bottom':265	
+	},
+	'flop5': {
+		'left':470,
+		'top':240,
+		'right':495,
+		'bottom':265	
+	}
+}
